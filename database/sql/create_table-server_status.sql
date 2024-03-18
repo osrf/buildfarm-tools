23e7e1c550f21766fd44986e1afc027a2f745f11
Jorge Perez <jjperez@ekumenlabs.com>
2024-03-18 16:25:49 -0300

    Add extra column project (#35)
    * Add extra column project
    * Update DB with added column
    
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>
    
    ---------
    
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- database/sql/create_table-server_status.sql
+++ database/sql/create_table-server_status.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS server_status(
     job_name TEXT PRIMARY KEY NOT NULL,
     server_status TEXT,
     release TEXT,
-    os_name TEXT
+    os_name TEXT,
+    project TEXT
 );
