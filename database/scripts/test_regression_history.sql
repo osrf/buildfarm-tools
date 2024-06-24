a5314c9ef3438bdeb607b1eddc620ceefeb9f784
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-06-24 07:20:25 -0500

    Restore test_regression_history script
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

---
+++ database/scripts/test_regression_history.sql
@@ -0,0 +1,13 @@
+SELECT test_failures.error_name,
+    test_failures.job_name,
+    test_failures.build_number,
+    build_status.build_datetime,
+    build_status.node_name
+FROM test_failures
+    INNER JOIN build_status ON (
+        test_failures.build_number = build_status.build_number
+        AND test_failures.job_name = build_status.job_name
+    )
+WHERE test_failures.error_name LIKE "@param1@"
+    AND test_failures.job_name LIKE "@param2@"
+ORDER BY build_status.build_datetime DESC;
