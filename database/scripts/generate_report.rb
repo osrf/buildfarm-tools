db93764950e2c5b0b6ed037ef26f192c19e2960a
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2024-06-27 16:37:30 -0300

    Change subsection names
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- database/scripts/generate_report.rb
+++ database/scripts/generate_report.rb
@@ -28,18 +28,19 @@ def generate_report(report_name, exclude_set)
     report = {
         'urgent' => {
             'build_regressions' => urgent_build_regressions = BuildfarmToolsLib::build_regressions_today(filter_known: true),
-            'test_regressions_consistent' => urgent_consistent_test_regressions = BuildfarmToolsLib::test_regressions_today(filter_known: true, only_consistent: true),
+            'test_regressions_consecutive' => urgent_consistent_test_regressions = BuildfarmToolsLib::test_regressions_today(filter_known: true, only_consistent: true),
             'test_regressions_flaky' => urgent_flaky_test_regressions = BuildfarmToolsLib::flaky_test_regressions(filter_known: true),
        },
        'maintenance' => {
             'jobs_failing' => [],
             'gh_issues_reported' => [],
-            'disabled_tests' => [],
+            'tests_disabled' => [],
        },
        'pending' => {
            'build_regressions_known' => [],
            'test_regressions_all' => [],
            'test_regressions_known' => [],
+           'build_regressions_known' => [],
        }
     }
 
