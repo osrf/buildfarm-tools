eee676c9e19bc31e76daba881c492420916b2968
Cristóbal Arroyo <cristobal.arroyo@ekumenlabs.com>
2024-07-23 17:01:03 -0500

    Don't filter known issues in report (#73)
    * Revert filter known issues
    
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>
    
    * Show flaky test regressions in details
    
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>
    
    ---------
    
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- database/scripts/generate_report.rb
+++ database/scripts/generate_report.rb
@@ -27,9 +27,9 @@ options[:report_name] = "buildfarm-report_#{DateTime.now.strftime("%Y-%m-%d_%H-%
 def generate_report(report_name, exclude_set)
     report = {
         'urgent' => {
-            'build_regressions' => urgent_build_regressions = BuildfarmToolsLib::build_regressions_today(filter_known: true),
-            'test_regressions_consecutive' => urgent_consistent_test_regressions = BuildfarmToolsLib::test_regressions_today(filter_known: true, only_consistent: true, group_issues: true),
-            'test_regressions_flaky' => urgent_flaky_test_regressions = BuildfarmToolsLib::flaky_test_regressions(filter_known: true, group_issues: true),
+            'build_regressions' => urgent_build_regressions = BuildfarmToolsLib::build_regressions_today(),
+            'test_regressions_consecutive' => urgent_consistent_test_regressions = BuildfarmToolsLib::test_regressions_today(only_consistent: true, group_issues: true),
+            'test_regressions_flaky' => urgent_flaky_test_regressions = BuildfarmToolsLib::flaky_test_regressions(group_issues: true),
        },
        'maintenance' => {
             'jobs_failing' => [],
