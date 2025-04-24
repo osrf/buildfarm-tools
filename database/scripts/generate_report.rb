f16979c71d8b4af1fbe7fb36a23e599c0b399456
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2025-04-24 08:10:20 -0500

    Apply rubocop format
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- database/scripts/generate_report.rb
+++ database/scripts/generate_report.rb
@@ -8,48 +8,50 @@ require 'ostruct'
 
 options = {}
 optparser = OptionParser.new do |o|
-    o.on "-e", "--exclude EXCLUDE_JOBS", "A list of string separated by spaces that represent the regex on a job name that you want to exclude" do |exclude|
-        options[:exclude] = Set.new(exclude.split(" "))
-    end
-    o.on "-o" "--output OUTPUT", "Name of output report" do |out|
-        options[:report_name] = out
-    end
-    o.on("-h", "--help", "Prints this help") do
-        puts o
-        exit
-    end
+  o.on '-e', '--exclude EXCLUDE_JOBS',
+       'A list of string separated by spaces that represent the regex on a job name that you want to exclude' do |exclude|
+    options[:exclude] = Set.new(exclude.split(' '))
+  end
+  o.on '-o', '--output OUTPUT', 'Name of output report' do |out|
+    options[:report_name] = out
+  end
+  o.on('-h', '--help', 'Prints this help') do
+    puts o
+    exit
+  end
 end
 
 optparser.parse!(ARGV)
-options[:exclude] = Set.new() if options[:exclude].nil?
-options[:report_name] = "buildfarm-report_#{DateTime.now.strftime("%Y-%m-%d_%H-%M")}.json" if options[:report_name].nil?
+options[:exclude] = Set.new if options[:exclude].nil?
+options[:report_name] = "buildfarm-report_#{DateTime.now.strftime('%Y-%m-%d_%H-%M')}.json" if options[:report_name].nil?
 
-def generate_report(report_name, exclude_set)
-    report_regressions_all = BuildfarmToolsLib::test_regressions_all(filter_known: true).freeze # Freeze as this is a read-only variable
-    report_regressions_consecutive = BuildfarmToolsLib::test_regressions_today(filter_known: true, only_consistent: true, group_issues: true, report_regressions: report_regressions_all)
-    report_flaky_regressions = BuildfarmToolsLib::flaky_test_regressions(filter_known: true, group_issues: true, report_regressions: report_regressions_all)
-    
-    report = {
-        'urgent' => {
-            'build_regressions' => BuildfarmToolsLib::build_regressions_today(filter_known: true),
-            'test_regressions_consecutive' => report_regressions_consecutive,
-            'test_regressions_flaky' => report_flaky_regressions,
-       },
-       'maintenance' => {
-            'jobs_last_success_date' => BuildfarmToolsLib::jobs_last_success_date(older_than_days: 7),
-            'gh_issues_reported' => [],
-            'tests_disabled' => [],
-       },
-       'pending' => {
-           'test_regressions_all' => BuildfarmToolsLib::test_regressions_all,
-           'test_regressions_known' => BuildfarmToolsLib::test_regressions_known,
-       }
+def generate_report(report_name, _exclude_set)
+  report_regressions_all = BuildfarmToolsLib.test_regressions_all(filter_known: true).freeze # Freeze as this is a read-only variable
+  report_regressions_consecutive = BuildfarmToolsLib.test_regressions_today(filter_known: true,
+                                                                            only_consistent: true, group_issues: true, report_regressions: report_regressions_all)
+  report_flaky_regressions = BuildfarmToolsLib.flaky_test_regressions(filter_known: true, group_issues: true,
+                                                                      report_regressions: report_regressions_all)
+
+  report = {
+    'urgent' => {
+      'build_regressions' => BuildfarmToolsLib.build_regressions_today(filter_known: true),
+      'test_regressions_consecutive' => report_regressions_consecutive,
+      'test_regressions_flaky' => report_flaky_regressions
+    },
+    'maintenance' => {
+      'jobs_last_success_date' => BuildfarmToolsLib.jobs_last_success_date(older_than_days: 7),
+      'gh_issues_reported' => [],
+      'tests_disabled' => []
+    },
+    'pending' => {
+      'test_regressions_all' => BuildfarmToolsLib.test_regressions_all,
+      'test_regressions_known' => BuildfarmToolsLib.test_regressions_known
     }
+  }
 
-
-    File.open(report_name, 'w') do |f|
-        f.write(report.to_json)
-    end
+  File.open(report_name, 'w') do |f|
+    f.write(report.to_json)
+  end
 end
 
 generate_report(options[:report_name], options[:exclude])
--- database/scripts/lib/report_formatter.rb
+++ database/scripts/lib/report_formatter.rb
@@ -3,11 +3,10 @@
 require 'json'
 
 module ReportFormatter
-
   TRACKED_PROJECTS = %w[ROS GAZEBO]
 
   def self.get_job_project(job_name)
-    project_name = ""
+    project_name = ''
     JOB_PROJECT_PATTERN.each_pair do |pattern, project|
       project_name = project if pattern.match? job_name
     end
@@ -24,23 +23,23 @@ module ReportFormatter
 
   def self.format_datetime(datetime)
     date, time = datetime.split
-    hour, minute, _ = time.split(':')
+    hour, minute, = time.split(':')
     "#{date} #{hour}:#{minute}"
   end
 
   def self.format_flakiness(flakiness_arr)
-    table_head = "<thead><tr><th>Job Name</th><th>Last Fail</th><th>First Fail</th><th>Build Count</th><th>Failure Count</th><th>Failure Percentage</th></tr></thead>"
-    table_body = ""
+    table_head = '<thead><tr><th>Job Name</th><th>Last Fail</th><th>First Fail</th><th>Build Count</th><th>Failure Count</th><th>Failure Percentage</th></tr></thead>'
+    table_body = ''
     flakiness_arr.each do |e|
       table_body += "<tr><td>#{e['job_name']}</td><td>#{e['last_fail']}</td><td>#{e['first_fail']}</td><td>#{e['build_count']}</td><td>#{e['failure_count']}</td><td>#{e['failure_percentage']}%</td></tr>"
     end
     table_body = "<tbody>#{table_body}</tbody>"
-    table = "<table>#{table_head}#{table_body}</table>"
-    table
+    "<table>#{table_head}#{table_body}</table>"
   end
 
   def self.build_regressions(br_array)
-    return "" if br_array.empty?
+    return '' if br_array.empty?
+
     table = "| Reference Build | Failure DateTime | Failure Reason |\n| -- | -- | -- |\n"
     br_array.each do |br_hash|
       reference_build = format_reference_build(br_hash)
@@ -50,14 +49,15 @@ module ReportFormatter
   end
 
   def self.test_regressions_consecutive(tr_array)
-    return "" if tr_array.empty?
+    return '' if tr_array.empty?
+
     table = "| Reference build | Age | Failure DateTime | Errors | Reports |\n| -- | -- | -- | -- | -- |\n"
     warnings_table = table
     tr_array.each do |tr_issue|
       reference_build = format_reference_build(tr_issue[0])
       age = tr_issue.first['age'].to_i
       failure_datetime = tr_issue.first['build_datetime']
-      errors = ""
+      errors = ''
       reports = []
       tr_issue.each do |e|
         errors += "<li>#{e['error_name']}</li>"
@@ -66,10 +66,10 @@ module ReportFormatter
       errors = "<ul>#{errors}</ul>"
 
       if reports.size > 0
-        reports_str = reports.uniq.map { |e| "<li>`#{e['github_issue']}` (#{e['status'].capitalize})</li>"}.join
+        reports_str = reports.uniq.map { |e| "<li>`#{e['github_issue']}` (#{e['status'].capitalize})</li>" }.join
         reports_str = "<ul>#{reports_str}</ul>"
       else
-        reports_str = "No reports found!"
+        reports_str = 'No reports found!'
       end
 
       # If output is too long, wrap it in a <details>
@@ -87,7 +87,8 @@ module ReportFormatter
   end
 
   def self.test_regressions_flaky(tr_array)
-    return "" if tr_array.empty?
+    return '' if tr_array.empty?
+
     table = "| Reference builds | Errors | Flaky report | Reports |\n| -- | -- | -- | -- |\n"
     warnings_table = table
     tr_array.each do |tr|
@@ -109,12 +110,12 @@ module ReportFormatter
       errors_str = "<details><summary>#{errors.size} items</summary>#{errors_str}</details>" if errors.size >= 10
 
       if reports.size > 0
-        reports_str = reports.uniq.map { |e| "<li>`#{e['github_issue']}` (#{e['status'].capitalize})</li>"}.join
+        reports_str = reports.uniq.map { |e| "<li>`#{e['github_issue']}` (#{e['status'].capitalize})</li>" }.join
         reports_str = "<ul>#{reports_str}</ul>"
       else
-        reports_str = "No reports found!"
+        reports_str = 'No reports found!'
       end
-      
+
       if tr.first['age'].to_i == -1
         warnings_table += "|#{jobs_str}|#{errors_str}|<details>#{format_flakiness(tr.first['flakiness'])}</details>|#{reports_str}|\n"
       else
@@ -127,7 +128,8 @@ module ReportFormatter
   end
 
   def self.jobs_last_success_date(job_array)
-    return "" if job_array.empty?
+    return '' if job_array.empty?
+
     table = "| Job Name | Last Success |\n| -- | -- |\n"
     job_array.each do |job|
       table += "| #{job['job_name']} | #{job['last_success']} |\n"
@@ -138,10 +140,9 @@ module ReportFormatter
   def self.test_regressions_all(test_regressions_array)
     table = "| Reference Build | Error Name |\n| -- | -- |\n"
     regressions = test_regressions_array.group_by { |e| e['job_name'] }
-    regressions.each_pair do |job, errors_hash|
-
+    regressions.each_pair do |_job, errors_hash|
       errors = errors_hash.map { |o| [o['error_name'], o['age']] }
-      errors_str = ""
+      errors_str = ''
       errors.each do |e|
         errors_str = "<ul><li>#{e.first} (age: #{e.last})</li><ul>"
       end
@@ -155,28 +156,29 @@ module ReportFormatter
   def self.test_regressions_known(issue_array)
     # Create a table for each project: {'ros' => {'open' => <table>, 'disabled' => <table>}, 'gazebo' => {'open' => <table>, 'disabled' => <table>}}
     table_template = "| Issue | Priority | Jobs Name | Errors Name |\n| -- | -- | -- | -- |\n"
-    table_by_status = {'open' => table_template, 'disabled' => table_template}
+    table_by_status = { 'open' => table_template, 'disabled' => table_template }
     tables = Hash[TRACKED_PROJECTS.zip([table_by_status, table_by_status.clone])]
 
     issue_array.each do |iss_report|
       jobs = iss_report.map { |o| o['job_name'] }.uniq
       jobs_str = "<ul><li>#{jobs.join('</li><li>')}</li></ul>"
       jobs_str = "<details><summary>#{jobs.size} jobs</summary>#{jobs_str}</details>" if jobs.size > 9
-      
+
       errors = iss_report.map { |o| o['error_name'] }.uniq
       errors_str = "<ul><li>#{errors.join('</li><li>')}</li></ul>"
       errors_str = "<details><summary>#{errors.size} errors</summary>#{errors_str}</details>" if errors.size > 9
-      
+
       # Add issue report data to it's respective project table
       project = iss_report.first['project']
       tables[project][iss_report.first['status'].downcase] += "| `#{iss_report.first['github_issue']}` | #{iss_report.first['priority']} | #{jobs_str} | #{errors_str} |\n"
     end
 
-    out = ""
+    out = ''
     tables.each_pair do |project, v|
       out += "### #{project.capitalize}\n"
       v.each_pair do |status, table|
         next if table == table_template
+
         out += "#### #{status.capitalize} issues\n"
         out += "#{table}\n"
       end
@@ -186,8 +188,9 @@ module ReportFormatter
 
   def self.format_report(report_hash)
     # Use <details> and <summary> tags to prevent long reports
-    details_subcategories = ['test_regressions_flaky', 'jobs_last_success_date', 'test_regressions_all', 'test_regressions_known']
-    output_report = ""
+    details_subcategories = %w[test_regressions_flaky jobs_last_success_date test_regressions_all
+                               test_regressions_known]
+    output_report = ''
 
     report_hash.each_pair do |category, subcategory_hash|
       output_report += "<h1>#{category.gsub('_', ' ').capitalize}</h1>\n"
@@ -196,7 +199,9 @@ module ReportFormatter
 
         subcategory_report_title = "<h2>#{subcategory.gsub('_', ' ').capitalize}</h2>\n"
         subcategory_report_str = "#{subcategory_report_title}\n#{subcategory_report}\n"
-        subcategory_report_str = "<details><summary>#{subcategory_report_title}</summary>\n\n#{subcategory_report}</details>\n" if details_subcategories.include? subcategory 
+        if details_subcategories.include? subcategory
+          subcategory_report_str = "<details><summary>#{subcategory_report_title}</summary>\n\n#{subcategory_report}</details>\n"
+        end
         output_report += subcategory_report_str
       end
     end
