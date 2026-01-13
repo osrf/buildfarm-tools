9b42a2f7d431b3c7345319238538d6d2c79958c5
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2026-01-13 10:32:48 -0500

    Add recent timeouts script
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

---
+++ database/scripts/recent_timeouts.sql
@@ -0,0 +1,12 @@
+SELECT build_status.job_name,
+	build_status.build_number,
+	build_status.build_datetime,
+	build_status.buildtime,
+	build_status.status
+FROM build_status
+	JOIN server_status ON build_status.job_name == server_status.job_name
+WHERE server_status.domain == 'https://ci.ros2.org' 
+	AND build_status.buildtime > 32400000 -- 9 hours
+	AND build_status.build_datetime > DATE('now',  '-60 days')
+ORDER BY build_status.build_datetime DESC;
+
