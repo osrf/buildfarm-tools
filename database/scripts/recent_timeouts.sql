012ebeb6266fdb5e79d8963659b1ec3b4f8db72f
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2026-01-13 10:37:35 -0500

    Parametrize domain
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- database/scripts/recent_timeouts.sql
+++ database/scripts/recent_timeouts.sql
@@ -5,7 +5,7 @@ SELECT build_status.job_name,
 	build_status.status
 FROM build_status
 	JOIN server_status ON build_status.job_name == server_status.job_name
-WHERE server_status.domain == 'https://ci.ros2.org' 
+WHERE server_status.domain == '@param1@'
 	AND build_status.buildtime > 32400000 -- 9 hours
 	AND build_status.build_datetime > DATE('now',  '-60 days')
 ORDER BY build_status.build_datetime DESC;
