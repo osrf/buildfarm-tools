83d820ced5a2ae570ed60e68a375c77dd93d4aad
miguelgonrod <miguel.gonzalez@ekumenlabs.com>
2026-02-09 10:42:08 -0500

    Moved the spreadsheet script
    Signed-off-by: miguelgonrod <miguel.gonzalez@ekumenlabs.com>

--- database/scripts/get_opened_known_issues.sql
+++ database/scripts/get_opened_known_issues.sql
@@ -1,10 +1,5 @@
-SELECT 
-    github_issue,
-    status,
-    assignee,
-    priority,
-    created_at,
-    issue_last_activity
+SELECT error_name,
+    github_issue
 FROM test_fail_issues
-WHERE status != "CLOSED"
+WHERE status = "OPEN"
 GROUP BY github_issue;
