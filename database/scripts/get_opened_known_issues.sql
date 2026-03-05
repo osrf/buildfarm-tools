e462f37a89b007d4a9341601f818b0f88622f70f
miguelgonrod <miguel.gonzalez@ekumenlabs.com>
2026-03-05 11:32:01 -0500

    Changed logic to check the not closed issues
    Signed-off-by: miguelgonrod <miguel.gonzalez@ekumenlabs.com>

--- database/scripts/get_opened_known_issues.sql
+++ database/scripts/get_opened_known_issues.sql
@@ -1,11 +1,9 @@
 SELECT error_name,
     github_issue
 FROM test_fail_issues
-WHERE status IN (
-    "BLOCKED",
-    "HELP WANTED",
-    "PENDING FIX",
-    "INVESTIGATING",
-    "NOT ASSIGNED"
+WHERE status NOT IN (
+    "COMPLETED",
+    "OBLIVIATED",
+    "DISABLED"
 )
 GROUP BY github_issue;
