c6c2cedb501dbc19ba81f748cfedd434bc74c68f
miguelgonrod <miguel.gonzalez@ekumenlabs.com>
2026-03-05 10:45:41 -0500

    Changed opened issues logic
    Signed-off-by: miguelgonrod <miguel.gonzalez@ekumenlabs.com>

--- database/scripts/get_opened_known_issues.sql
+++ database/scripts/get_opened_known_issues.sql
@@ -1,5 +1,11 @@
 SELECT error_name,
     github_issue
 FROM test_fail_issues
-WHERE status = "OPEN"
+WHERE status IN (
+    "BLOCKED",
+    "HELP WANTED",
+    "PENDING FIX",
+    "INVESTIGATING",
+    "NOT ASSIGNED"
+)
 GROUP BY github_issue;
