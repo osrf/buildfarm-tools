15dfd2a0cc4d095c067ff77c2d7e0732c19e1620
miguelgonrod <miguel.gonzalez@ekumenlabs.com>
2026-03-13 12:30:47 -0500

    Added parameters needed for report
    Signed-off-by: miguelgonrod <miguel.gonzalez@ekumenlabs.com>

--- database/scripts/get_closed_known_issues.sql
+++ database/scripts/get_closed_known_issues.sql
@@ -1,9 +1,13 @@
 SELECT error_name,
-    github_issue
+    github_issue,
+    status,
+    job_name,
+    server_status.project
 FROM test_fail_issues
+LEFT JOIN server_status ON test_fail_issues.job_name = server_status.job_name
 WHERE status IN (
     "COMPLETED",
     "OBLIVIATED",
     "DISABLED"
 )
-GROUP BY github_issue;
+GROUP BY github_issue, job_name;
