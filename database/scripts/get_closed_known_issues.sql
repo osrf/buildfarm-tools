50e9d8fccf24ab6de9866ad907f6aad66a7e7a81
miguelgonrod <miguel.gonzalez@ekumenlabs.com>
2026-03-13 14:54:59 -0500

    Added parameters table
    Signed-off-by: miguelgonrod <miguel.gonzalez@ekumenlabs.com>

--- database/scripts/get_closed_known_issues.sql
+++ database/scripts/get_closed_known_issues.sql
@@ -1,13 +1,13 @@
-SELECT error_name,
-    github_issue,
-    status,
-    job_name,
+SELECT test_fail_issues.error_name,
+    test_fail_issues.github_issue,
+    test_fail_issues.status,
+    test_fail_issues.job_name,
     server_status.project
 FROM test_fail_issues
 LEFT JOIN server_status ON test_fail_issues.job_name = server_status.job_name
-WHERE status IN (
+WHERE test_fail_issues.status IN (
     "COMPLETED",
     "OBLIVIATED",
     "DISABLED"
 )
-GROUP BY github_issue, job_name;
+GROUP BY test_fail_issues.github_issue, test_fail_issues.job_name;
